33967086d810bc1a28f80be9602189bec5672a9e
Tony Tran <s3604730@student.rmit.edu.au>
2019-03-27 15:28:17 +1100

    Just adding extra comments

--- classList/classSensor.py
+++ classList/classSensor.py
@@ -1,10 +1,12 @@
 #thinking about doing this as an abstract for temperature and humidity.
 from abc import ABC, ABCMeta, abstractmethod
 
+#abstract method
 class ClassSensor(ABC):
     def __init__(self):
         pass
-
+    #abstract method that both the temperature and humidity classes 
+    #will use
     @abstractmethod
     def returnValue(self):
         pass
--- classList/classTemperature2.py
+++ classList/classTemperature2.py
@@ -1,11 +1,11 @@
 from classList.classSensor import ClassSensor
 from classList.virtual_sense_hat import VirtualSenseHat
 
-
+#using the reference form abstract class ClassSensor
 class ClassTemperature2(ClassSensor):
     def __init__(self):
         pass
-        
+        #abstract method in classSensor
     def returnValue(self):
         __sense = VirtualSenseHat.getSenseHat()
         __temperature = __sense.get_temperature()
