4b1630f4d25863a5e6f0cc5c4cbc07a9fa793777
Tony Tran <s3604730@student.rmit.edu.au>
2019-04-07 13:15:31 +1000

    Fixing up analytics comments and redundant code

--- classList/seabornHumidity.py
+++ classList/seabornHumidity.py
@@ -19,9 +19,5 @@ class SeaBornHumidity():
  def outputHumidity(self):
  #read csv  
   dff = pd.read_csv("analytics.csv")
-  #sns_plot = sns.distplot(dff['humidity'])
-  #sns_plot.figure.savefig("humidityHist.png")
-  #fig = sns_plot.get_figure()
-  #fig.savefig('humidityHist.png')
   ax = sns.countplot(x = dff['humidity'])
-  ax.figure.savefig("humidityHist.png")
+  ax.figure.savefig("humidityBar.png")
